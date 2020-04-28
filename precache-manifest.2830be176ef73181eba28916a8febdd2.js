7ba0fc7f59f1564060c12a79838fa2cd66b0c7cf
hipek <puaro@vp.pl>
2020-04-28 18:06:18 +0200

    Updates

---
+++ precache-manifest.2830be176ef73181eba28916a8febdd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcecb2399c9ea641fb9fb4469af96bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7645c0816254d8335a5a",
+    "url": "/static/css/main.a04db7ce.chunk.css"
+  },
+  {
+    "revision": "a11dbceaa4554b68a14c",
+    "url": "/static/js/2.be56a0c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be56a0c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7645c0816254d8335a5a",
+    "url": "/static/js/main.6a020f33.chunk.js"
+  },
+  {
+    "revision": "d77960c1a3d7e29c7d81",
+    "url": "/static/js/runtime-main.4e1cf048.js"
+  },
+  {
+    "revision": "f66ce24a2e2952892ca3069f2b6eb1ea",
+    "url": "/static/media/sowa.f66ce24a.jpg"
+  }
+]);
